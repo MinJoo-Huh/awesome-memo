905faa7f556c84b1d3223bb668fc44167f9b9554
Minjoo Huh <huhminjoo@naver.com>
2024-08-14 16:04:10 +0900

    ⭐글쓰기 기능 구현
    -프론트엔드 글쓰기 페이지 form으로 작업했습니다.
    - 백엔드와 SQLite를 연동해서 이미지 및 데이터를 업로드하는 기능을 구현했습니다.

--- main.py
+++ main.py
@@ -2,21 +2,47 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
+
 class Memo(BaseModel):
-    id:int
+    id:str
     content:str
     
 memos = []
 
 app = FastAPI()
 
+# 메모 서버에 저장하기
 @app.post("/memos")
-def create_memo(memo:Memo):
+def create_memo(memo:Memo): # Memo의 형태의 변수 memo에 저장.
     memos.append(memo)
     return '메모 추가에 성공하셨습니다'
 
+# 메모 서버에서 읽어오기
 @app.get("/memos")
 def read_memo():
     return memos
+
+# 서버에 저장되어 있는 메모 수정하기
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content    # 배열 내용 수정하기
+            return "성공했습니다."
+    return "그런 메모는 없습니다."
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    for index, memo in enumerate(memos):    # index와 같이 값을 봅아주게 되는?
+        print(index, "for문에 들어와있다.", memo.id)
+        print("받은 id는 : ", memo_id)
+        
+        if memo.id == memo_id:
+            memos.pop(index)
+            return "삭제를 완료하였습니다"
+    return "그런 메모는 없습니다."
+
+
+# 서버를 수정하면 데이터가 날아가면서 초기화됨.
     
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,10 +1,57 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id; // 어떤 것에서 이벤트가 일어났는지
+  const editInput = prompt("수정할 값을 입력하세요");
+
+  // path로 특정 id를 보낼려고 하고 있다.
+  const res = await fetch(`/memos/${id}`, {
+    // request-body
+    method: "PUT", // 수정할 때 PUT 사용. 특정 값이 있을 때 이걸로 바꿔줘 method
+    headers: {
+      // post로 request body에 보내는데 필수요건
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // 보내는건 문자열만 받는 건 JSON으로 바꿔줌
+      id, // id(: id), 생략 가능 - int로 보내자...
+      content: editInput,
+    }),
+  });
+  readMemo(); //확인용
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id; // 어떤 것에서 이벤트가 일어났는지
+  //console.log("deletemomo : ", event.target.dataset.id);
+
+  // path로 특정 id를 보낼려고 하고 있다.
+  const res = await fetch(`/memos/${id}`, {
+    // request-body
+    method: "DELETE", // header, body 보낼 필요 X? id 만으로도 지우는 건 알 수 있으니깐
+  });
+  readMemo(); //확인용
+}
+
 function displayMemos(memo) {
+  // 잃어온 후 화면에 생성 (list)
   const ul = document.querySelector("#memo-ul");
   const li = document.createElement("li");
-
   li.innerText = `[id:${memo.id}] ${memo.content}`;
 
+  // update 버튼 추가
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id; // html에 값을 넣어줄때, html에서 data-id=""이런 것처럼 자바스크립트에서 넣음
+
+  // delete 버튼 추가
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
   ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
 }
 
 // 메모리 읽어오기
@@ -33,7 +80,7 @@ async function createMemo(value) {
     },
     body: JSON.stringify({
       // 보내는건 문자열만 받는 건 JSON으로 바꿔줌
-      id: new Date().getTime(),
+      id: new Date().getTime().toString(),
       content: value,
     }),
   });
